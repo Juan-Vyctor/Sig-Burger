90fa10357e17182ff6f7a21ab1772ade85489d90
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2025-08-14 09:44:38 -0300

    feat: add navegacao inicial

--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello World.");
+int main(void)
+{
+    printf("Bem vindo a Sig Burguers!");
 
+    int operacao_principal;
+    do {
+        printf("\nDigite 1 para o cardápio, 2 para um pedido, 3 para o histórico, 4 para nosso Sobre, e 0 para finalizar:\n");
+        scanf("%d", &operacao_principal);
+        printf("\n");
+
+        if (operacao_principal == 1)
+        {
+            printf("Aqui está o Cardápio");
+        }
+        else if (operacao_principal == 2)
+        {
+            printf("Faça o seu pedido");
+        }
+        else if (operacao_principal == 3)
+        {
+            printf("Aqui está o histórico dos seus pedidos");
+        } else if (operacao_principal == 4) {
+            printf("Sobre nós");
+        } else if (operacao_principal != 0) {
+            printf("Está opção é inválida, retornando ao menu inicial...");
+        }
+    } while (operacao_principal != 0);
+
+    printf("Obrigado por ter utilizado nosso sistema!");
     return 0;
 }
