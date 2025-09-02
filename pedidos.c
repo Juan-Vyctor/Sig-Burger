0fa0eb0453c226a09f02b9c450ce5ec203423a7d
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2025-09-02 13:37:36 -0300

    refactor: move funcoes para modulo de pedido

---
+++ pedidos.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "modulos.h"
+
+void tela_informacoes_pedido(void)
+{
+    system("clear");
+    printf("////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                          ///\n");
+    printf("///                   = = = = = Sig - Burguer = = = = =                      ///\n");
+    printf("///                                                                          ///\n");
+    printf("///                        informações do seu pedido                         ///\n");
+    printf("///                                                                          ///\n");
+    printf("///   Para realizar seu pedido, informe:                                     ///\n");
+    printf("///                                                                          ///\n");
+    printf("///   - Endereço de entrega (rua, número, bairro, complemento).              ///\n");
+    printf("///   - Número(s) do(s) item(ns) desejado(s) do cardápio.                    ///\n");
+    printf("///   - Nome do cliente para registrar o pedido.                             ///\n");
+    printf("///                                                                          ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+void tela_informar_item(void)
+{
+    system("clear");
+    printf("////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                          ///\n");
+    printf("///                   = = = = = Sig - Burguer = = = = =                      ///\n");
+    printf("///                                                                          ///\n");
+    printf("///                 Digite o número do item que deseja                       ///\n");
+    printf("///                                                                          ///\n");
+    printf("///   1. Clássico Burguer ................................. R$ 18,90         ///\n");
+    printf("///   2. Cheddar Bacon .................................... R$ 22,50         ///\n");
+    printf("///   3. Duplo Smash ...................................... R$ 25,00         ///\n");
+    printf("///   4. Veggie Burguer ................................... R$ 20,00         ///\n");
+    printf("///   5. Frango Crispy .................................... R$ 21,50         ///\n");
+    printf("///   6. Super Burguer (artesanal 200g) ................... R$ 27,90         ///\n");
+    printf("///                                                                          ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
