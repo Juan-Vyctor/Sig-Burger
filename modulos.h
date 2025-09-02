271b731c8fbaa418ddd30ea6457fadedc4cae825
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2025-09-02 13:29:57 -0300

    refactor: add funcoes de clientes no header

--- modulos.h
+++ modulos.h
@@ -7,4 +7,11 @@
     void tela_produto(void);
     void tela_adicionar_produto(void);
 
+    // Funções do módulo de Cliente
+    void tela_menu_clientes(void);
+    void tela_cadastrar_cliente(void);
+    void tela_listar_clientes(void);
+    void tela_visualizar_cliente(void);
+    void tela_cadastrar_endereco(void);
+    void tela_visualizar_endereco(void);
 #endif
