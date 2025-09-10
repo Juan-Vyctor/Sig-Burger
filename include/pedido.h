2922d1a0fcbb2403a8209693feb3903e4ad03d80
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2025-09-10 16:07:29 -0300

    refactor: divide modulos

---
+++ include/pedido.h
@@ -0,0 +1,6 @@
+#ifndef PEDIDO_H
+    #define PEDIDO_H
+
+    void tela_informacoes_pedido(void);
+    void tela_informar_item(void);
+#endif
--- pedido.c
+++ pedido.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "modulos.h"
+#include "./include/pedido.h"
 
 void tela_informacoes_pedido(void)
 {
