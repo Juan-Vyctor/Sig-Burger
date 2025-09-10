639dbed7561a38e752ce5263934f2d0cf3c0b634
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2025-09-10 16:07:13 -0300

    refactor: divide modulos

---
+++ include/cliente.h
@@ -0,0 +1,11 @@
+#ifndef CLIENTE_H
+    #define CLIENTE_H
+    
+    void modulo_cliente(void);
+    int tela_menu_clientes(void);
+    void tela_cadastrar_cliente(void);
+    void tela_listar_clientes(void);
+    void tela_visualizar_cliente(void);
+    void tela_cadastrar_endereco(void);
+    void tela_visualizar_endereco(void);
+#endif
