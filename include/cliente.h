6cca35f15b9fba73a0af004ad60a81aac89afb55
Luisfelipelinhares <luis.felipe.linhares.701@ufrn.edu.br>
2025-09-29 19:35:03 +0000

    refactor: refeito o arquivo cliente.h

--- include/cliente.h
+++ include/cliente.h
@@ -4,9 +4,9 @@
     void modulo_cliente(void);
     int tela_menu_clientes(void);
     void tela_cadastrar_cliente(void);
+    void tela_deletar_cliente(void);
     void tela_listar_clientes(void);
     void tela_visualizar_cliente(void);
-    void tela_cadastrar_endereco(void);
-    void tela_visualizar_endereco(void);
+    void tela_atualizar_cliente(void);
     
 #endif
