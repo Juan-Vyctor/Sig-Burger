e065b0db26ff9ac4df149d6495c0b8e71350c6e9
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-11-10 21:23:13 -0300

    feat: adicionando mais assinaturas de funções ao header file

--- include/relatorio.h
+++ include/relatorio.h
@@ -4,5 +4,8 @@
 void menu_relatorio();
 void relatorio();
 void listar_todos_clientes();
+void listar_todos_funcionarios();
+void listar_todos_produtos();
+void listar_todos_enderecos();
 
 #endif // RELATORIO_H
